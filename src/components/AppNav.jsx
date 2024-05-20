1286365337eae75e75309bddc74c0e2ddb21f1cc
kennykwyiu <kenny.kwyiu@gmail.com>
2024-05-20 21:52:12 +0800

    212-created NavLink in AppNav

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,7 +1,22 @@
+import { NavLink } from "react-router-dom";
 import style from "./AppNav.module.css";
 
 function AppNav() {
-  return <nav className={style.nav}>App navigation</nav>;
+  return (
+    <nav className={style.nav}>
+      <ul>
+        <li>
+          <NavLink to="cities">Cities</NavLink>
+        </li>
+        <li>
+          <NavLink to="countries">Countries</NavLink>
+        </li>
+        {/* <li>
+          <NavLink to="form">Form</NavLink>
+        </li> */}
+      </ul>
+    </nav>
+  );
 }
 
 export default AppNav;
