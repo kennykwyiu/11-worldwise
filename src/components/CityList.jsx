4595aae5743b7543ae69844cf1ddf6e912a29cde
kennykwyiu <kenny.kwyiu@gmail.com>
2024-05-22 23:22:10 +0800

    213-fixed bug

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -8,7 +8,7 @@ function CityList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
 
   return (
-    <ul className={styles.CityList}>
+    <ul className={styles.cityList}>
       {cities.map((city) => (
         <CityItem city={city} key={city.id} />
       ))}
