099f280824fea059b4144076623ce9a06f8c1587
kennykwyiu <kenny.kwyiu@gmail.com>
2024-05-25 12:08:00 +0800

    215-added param into url

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -44,11 +45,13 @@ function App() {
             <Route
               index
               element={<CityList cities={cities} isLoading={isLoading} />}
-            />{" "}
+            />
+
             <Route
               path="cities"
               element={<CityList cities={cities} isLoading={isLoading} />}
             />
+            <Route path="cities/:id" element={<City />} />
             <Route
               path="countries"
               element={<CountryList cities={cities} isLoading={isLoading} />}
