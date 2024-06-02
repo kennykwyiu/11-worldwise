bd72ac4f31044f1d700ad9be1081317504dc436e
kennykwyiu <kenny.kwyiu@gmail.com>
2024-06-02 16:57:48 +0800

    229-add back the code

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
-// import styles from "./City.module.css";
+import styles from "./City.module.css";
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 
@@ -27,54 +27,42 @@ function City() {
     [id]
   );
 
-  // TEMP DATA
-  // const currentCity = {
-  //   cityName: "Lisbon",
-  //   emoji: "🇵🇹",
-  //   date: "2027-10-31T15:59:59.138Z",
-  //   notes: "My favorite city so far!",
-  // };
-
   const { cityName, emoji, date, notes } = currentCity;
 
   return (
-    <>
-      <h1>City {id}</h1>
-      <p>Position:</p>
-    </>
-    // <div className={styles.city}>
-    //   <div className={styles.row}>
-    //     <h6>City name</h6>
-    //     <h3>
-    //       <span>{emoji}</span> {cityName}
-    //     </h3>
-    //   </div>
+    <div className={styles.city}>
+      <div className={styles.row}>
+        <h6>City name</h6>
+        <h3>
+          <span>{emoji}</span> {cityName}
+        </h3>
+      </div>
 
-    //   <div className={styles.row}>
-    //     <h6>You went to {cityName} on</h6>
-    //     <p>{formatDate(date || null)}</p>
-    //   </div>
+      <div className={styles.row}>
+        <h6>You went to {cityName} on</h6>
+        <p>{formatDate(date || null)}</p>
+      </div>
 
-    //   {notes && (
-    //     <div className={styles.row}>
-    //       <h6>Your notes</h6>
-    //       <p>{notes}</p>
-    //     </div>
-    //   )}
+      {notes && (
+        <div className={styles.row}>
+          <h6>Your notes</h6>
+          <p>{notes}</p>
+        </div>
+      )}
 
-    //   <div className={styles.row}>
-    //     <h6>Learn more</h6>
-    //     <a
-    //       href={`https://en.wikipedia.org/wiki/${cityName}`}
-    //       target="_blank"
-    //       rel="noreferrer"
-    //     >
-    //       Check out {cityName} on Wikipedia &rarr;
-    //     </a>
-    //   </div>
+      <div className={styles.row}>
+        <h6>Learn more</h6>
+        <a
+          href={`https://en.wikipedia.org/wiki/${cityName}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Check out {cityName} on Wikipedia &rarr;
+        </a>
+      </div>
 
-    //   <div></div>
-    // </div>
+      <div></div>
+    </div>
   );
 }
 
