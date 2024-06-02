48029ba040660b97507ec7588581c5085c592614
kennykwyiu <kenny.kwyiu@gmail.com>
2024-06-02 15:45:22 +0800

    227-created CitiesContext

--- src/App.jsx
+++ src/App.jsx
@@ -12,28 +12,7 @@ import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
 
-const BASE_URL = "http://localhost:9000";
-
 function App() {
-  const [cities, setCities] = useState([]); // eslint-disable-line no-unused-vars
-  const [isLoading, setIsLoading] = useState(false); // eslint-disable-line no-unused-vars
-
-  useEffect(function () {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-        setCities(data);
-      } catch {
-        alert("There was an error loading data...");
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchCities();
-  }, []);
-
   return (
     <div>
       <BrowserRouter>
---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useEffect, useState } from "react";
+
+const CitiesContext = createContext();
+const BASE_URL = "http://localhost:9000";
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]); // eslint-disable-line no-unused-vars
+  const [isLoading, setIsLoading] = useState(false); // eslint-disable-line no-unused-vars
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+}
