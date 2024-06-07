66ca776f23620bfabc59eba73466fe5b0ddd24c4
kennykwyiu <kenny.kwyiu@gmail.com>
2024-06-07 21:28:45 +0800

    229-implemented green highLght on the selected city box

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -2,6 +2,7 @@
 /* eslint-disable no-unused-vars */
 import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
+import { useCities } from "../contexts/CitiesContext";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -11,6 +12,7 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
+  const { currentCity } = useCities();
   const { cityName, emoji, date, id, position } = city;
 
   const flagemojiToPNG = (flag) => {
@@ -25,7 +27,9 @@ function CityItem({ city }) {
   return (
     <li>
       <Link
-        className={styles.cityItem}
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles["cityItem--active"] : ""
+        }`}
         to={`${id}?lat=${position.lat}&lng=${position.lng}`}
       >
         <span className={styles.emoji}>{flagemojiToPNG(emoji)}</span>
