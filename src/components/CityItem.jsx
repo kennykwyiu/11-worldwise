ec24280c04ecb3133a6ef7cf43a1a470618e292c
kennykwyiu <kenny.kwyiu@gmail.com>
2024-06-11 21:24:59 +0800

    236-finished deleting a city

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -12,7 +12,7 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
-  const { currentCity } = useCities();
+  const { currentCity, deleteCity } = useCities();
   const { cityName, emoji, date, id, position } = city;
 
   const flagemojiToPNG = (flag) => {
@@ -26,7 +26,7 @@ function CityItem({ city }) {
 
   function handleClick(e) {
     e.preventDefault();
-    console.log("TEST");
+    deleteCity(id);
   }
 
   return (
