fb34b6023c4c7dcd7e8b917332d544e85bcdf758
kennykwyiu <kenny.kwyiu@gmail.com>
2024-06-15 17:26:11 +0800

    238-implemented login with Fake user

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -26,13 +26,27 @@ function reducer(state, action) {
   }
 }
 
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
 function AuthProvider({ children }) {
   const [{ user, isAuthenticated }, dispatch] = useReducer();
-  function login(email, password) {}
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password)
+      dispatch({ type: "login", payload: FAKE_USER });
+  }
 
   function logout() {}
 
-  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 function useAuth() {
