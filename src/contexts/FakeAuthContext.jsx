a2aef6fd9aea28a27acecbac834e38e1a22f0c81
kennykwyiu <kenny.kwyiu@gmail.com>
2024-06-15 17:20:16 +0800

    238-added 2 cases in reducer in AuthContext

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -2,6 +2,30 @@ import { createContext, useContext, useReducer } from "react";
 
 const AuthContext = createContext();
 
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return {
+        ...state,
+        user: action.payload,
+        isAuthenticated: true,
+      };
+    case "logout":
+      return {
+        ...state,
+        user: null,
+        isAuthenticated: false,
+      };
+    default:
+      throw new Error("Unknow action");
+  }
+}
+
 function AuthProvider({ children }) {
   const [{ user, isAuthenticated }, dispatch] = useReducer();
   function login(email, password) {}
