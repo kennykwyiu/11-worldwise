5715529966c41361bd086748a38801828dc988ff
kennykwyiu <kenny.kwyiu@gmail.com>
2024-06-16 14:15:07 +0800

    240-finshed and fixed bugs

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -13,7 +13,7 @@ function ProtectedRoute({ children }) {
     [isAuthenticated, navigate]
   );
 
-  return children;
+  return isAuthenticated ? children : null;
 }
 
 export default ProtectedRoute;
